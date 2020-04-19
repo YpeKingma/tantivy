1dbfbdb342c6762143f62b22a339bee15476443d
Ype Kingma <ykingma@xs4all.nl>
2020-04-19 16:38:05 +0200

    Add fill_buffer to TwoPhaseApproximation.

--- src/query/twophasedocset.rs
+++ src/query/twophasedocset.rs
@@ -1,4 +1,5 @@
 use crate::docset::{DocSet, SkipResult};
+use crate::common::BitSet;
 use crate::DocId;
 
 struct TwoPhaseApproximation<TDocSet: DocSet> {
@@ -20,7 +21,7 @@ pub trait TwoPhaseDocSet: DocSet {
     // Returns an expected cost in number of simple operations like addition, multiplication,
     // comparing two numbers and indexing an array.
     // The returned value must be positive.
-    fn match_cost() -> f32;
+    fn match_cost(self) -> f32;
 
     // Return whether the current valid doc in the approximating DocSet is on a match.
     // This should only be called when the DocSet is positioned, and at most once.
@@ -29,7 +30,7 @@ pub trait TwoPhaseDocSet: DocSet {
 }
 
 impl<TDocSet: DocSet> DocSet for TwoPhaseApproximation<TDocSet> {
-    // much like ConstScorer in scorer
+    // Much like ConstScorer in scorer. CHECKME: avoid this almost duplication?
     fn advance(&mut self) -> bool {
         self.approximation.advance()
     }
@@ -42,7 +43,15 @@ impl<TDocSet: DocSet> DocSet for TwoPhaseApproximation<TDocSet> {
         self.approximation.doc()
     }
 
+    fn fill_buffer(&mut self, buffer: &mut [DocId]) -> usize {
+        self.approximation.fill_buffer(buffer)
+    }
+
     fn size_hint(&self) -> u32 {
         self.approximation.size_hint()
+    }
+
+    fn append_to_bitset(&mut self, bitset: &mut BitSet) {
+        self.approximation.append_to_bitset(bitset);
     }
 }
